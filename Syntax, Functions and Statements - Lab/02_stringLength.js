5055bf1fe28420df60ab930edba0b6df7e9a3280
dsimplichev <dsimplichev1948@gmail.com>
2023-05-24 22:32:59 +0100

    Syntax, Function and Statements

---
+++ Syntax, Functions and Statements - Lab/02_stringLength.js
@@ -0,0 +1,15 @@
+function stringLength(input, input2, input3){
+
+let total = 0
+total += input.length;
+total += input2.length;
+total += input3.length;
+
+let avg = Math.floor(total/3);
+
+console.log(total)
+console.log(avg)
+
+
+}
+stringLength('chocolate', 'ice cream', 'cake' )
---
+++ Syntax, Functions and Statements - Lab/03_largestNumber.js
@@ -0,0 +1,12 @@
+function largestNumber(num1, num2, num3) {
+
+
+let biggestNum = Math.max(num1, num2, num3);
+
+console.log(`The largest number is ${biggestNum}.`);
+
+
+
+
+}
+largestNumber(5, -3, 16)
