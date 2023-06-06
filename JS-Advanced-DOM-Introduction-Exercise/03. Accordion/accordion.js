a44962acef1b9bc37fe59de0ef58d32990579c31
dsimplichev <dsimplichev1948@gmail.com>
2023-06-06 19:25:30 +0100

    DOM Exercise

--- JS-Advanced-DOM-Introduction-Exercise/03. Accordion/accordion.js
+++ JS-Advanced-DOM-Introduction-Exercise/03. Accordion/accordion.js
@@ -1,3 +1,16 @@
 function toggle() {
-    console.log('TODO:...');
+    let buttonInfo = document.querySelector('.button');
+    const text = document.getElementById('extra');
+
+    if (buttonInfo.textContent == 'More') {
+
+        buttonInfo.textContent = 'Less';
+        text.style.display = 'block';
+
+    } else if (buttonInfo.textContent == 'Less') {
+
+        buttonInfo.textContent = 'More';
+        text.style.display = 'none';
+
+    }
 }
--- JS-Advanced-DOM-Introduction-Exercise/04. Search in List/search.js
+++ JS-Advanced-DOM-Introduction-Exercise/04. Search in List/search.js
@@ -1,3 +1,18 @@
 function search() {
-   // TODO
+   let towns = Array.from((document.getElementById('towns').children));
+   let searchBar = document.getElementById('searchText').value;
+   let result = document.getElementById('result');
+   let count = 0;
+   
+
+   towns.map(town =>{
+      if(town.textContent.includes(searchBar)){
+         town.style.textDecoration = 'underline';
+         town.style.fontWeight = 'bold';
+         count++;
+      }
+   });
+
+result.textContent = `${count} matches found`
+
 }
