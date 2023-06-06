7028a0be80f3254ded294c5183f3480a8536e3ec
dsimplichev <dsimplichev1948@gmail.com>
2023-06-06 19:40:39 +0100

    DOM Exercise

--- JS-Advanced-DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
+++ JS-Advanced-DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,14 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   let table = Array.from(document.querySelector('.container').querySelectorAll('tbody tr'));
 
    function onClick() {
-      //   TODO:
-
+      table.map(r => r.classList.remove('select'));
+      const searchBar = document.getElementById('searchField').value;
+      table.map( row => {
+         if(row.textContent.includes(searchBar)) {
+            row.classList.add('select');
+         }
+      });
    }
 }
